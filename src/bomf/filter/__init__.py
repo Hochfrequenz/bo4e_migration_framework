ea9bd88aff445f3fa1f1408926f41fb2525ec1bd
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-13 11:32:19 +0100

    ✨ Introduce `AggregateFilter`: A structured way to filter items based on aggregate properties
    without loosing logging transparency

--- src/bomf/filter/__init__.py
+++ src/bomf/filter/__init__.py
@@ -50,3 +50,52 @@ class Filter(ABC, Generic[Candidate]):
             "%i out of %i candidates have been removed by the filter", candidates_removed, len(candidates)
         )
         return result
+
+
+Aggregate = TypeVar("Aggregate")
+"""
+Aggregate is a type that enhances the candidate with some additional information used by a Filter on the aggregate.
+It has to be possible to extract the candidate from the aggregate.
+"""
+
+
+class AggregateFilter(ABC, Generic[Candidate, Aggregate]):
+    """
+    A filter that takes objects from candidates and returns only those that are relevant for its use case.
+    The difference to the plain Filter is, that the conditions can be defined on groups of candidates instead of
+    isolated single candidates. Still it (other than e.g. a usual groupby+filter) logs those entries that do not pass
+    the filter.
+    """
+
+    def __init__(self, base_filter: Filter[Aggregate]):
+        """
+        Instantiate by providing a filter that is applied on the aggregate
+        """
+        self._logger = logging.getLogger(self.__module__)
+        self._base_filter = base_filter
+
+    @abstractmethod
+    async def aggregate(self, candidates: List[Candidate]) -> List[Aggregate]:
+        """
+        Create aggregates which are then passed to the base filter that works on the aggregate.
+        The method is async so that you can do complex (and e.g. network based) aggregations.
+        """
+        raise NotImplementedError("The inheriting class has to implement this method")
+
+    @abstractmethod
+    def disaggregate(self, aggregate: Aggregate) -> Candidate:
+        """
+        extract a single candidate from the aggregate that passed the filter
+        """
+        raise NotImplementedError("The inheriting class has to implement this method")
+
+    async def apply(self, candidates: List[Candidate]) -> List[Candidate]:
+        """
+        If aggregate and disaggregate and the base_filter are properly setup, then apply will filter the list of
+        candidates based on the aggregate base filter.
+        """
+        aggregates = await self.aggregate(candidates)
+        self._logger.info("There are %i candidates and %i aggregates", len(candidates), len(aggregates))
+        filtered_aggregates = await self._base_filter.apply(aggregates)
+        self._logger.info("There are %i filtered aggregates left", len(filtered_aggregates))
+        return [self.disaggregate(fa) for fa in filtered_aggregates]
--- unittests/test_filter.py
+++ unittests/test_filter.py
@@ -1,9 +1,11 @@
+import dataclasses
 import logging
+from itertools import groupby
 from typing import List
 
 import pytest  # type:ignore[import]
 
-from bomf.filter import Filter
+from bomf.filter import AggregateFilter, Filter
 
 
 class _FooFilter(Filter):
@@ -27,3 +29,72 @@ class TestFilter:
         actual = await filter_under_test.apply(candidates)
         assert actual == survivors
         assert "1 out of 2 candidates have been removed by the filter" in caplog.messages
+
+
+@dataclasses.dataclass
+class _MyCandidate:
+    number: int
+    string: str
+
+
+@dataclasses.dataclass
+class _MyAggregate:
+    group_key: str
+    max_number_for_key: int
+    candidate: _MyCandidate
+
+
+class _BarFilter(AggregateFilter):
+    """
+    An Aggregate Filter that groups _MyCandidates by their string attribute and keeps only those entries that have the
+    highest number (attribute) in their respective group.
+    It's basically a show-case test that allows to understand how the aggregate filters are supposed to be used.
+    """
+
+    def __init__(self):
+        class _BaseFilter(Filter[_MyAggregate]):
+            async def predicate(self, candidate: _MyAggregate) -> bool:
+                return candidate.max_number_for_key == candidate.candidate.number
+
+        base_filter = _BaseFilter()
+        super(_BarFilter, self).__init__(base_filter)
+
+    async def aggregate(self, candidates: List[_MyCandidate]) -> List[_MyAggregate]:
+        result: List[_MyAggregate] = []
+        for group_key, group in groupby(sorted(candidates, key=lambda c: c.string), lambda c: c.string):
+            group_items = list(group)
+            max_number_in_group = max(group_item.number for group_item in group_items)
+            for group_item in group_items:
+                result.append(
+                    _MyAggregate(group_key=group_key, max_number_for_key=max_number_in_group, candidate=group_item)
+                )
+        return result
+
+    def disaggregate(self, aggregate: _MyAggregate) -> _MyCandidate:
+        return aggregate.candidate
+
+
+class TestAggregateFilter:
+    @pytest.mark.parametrize(
+        "filter_under_test,candidates,survivors",
+        [
+            pytest.param(
+                _BarFilter(),
+                [
+                    _MyCandidate(number=1, string="foo"),
+                    _MyCandidate(number=19, string="bar"),
+                    _MyCandidate(number=2, string="foo"),
+                    _MyCandidate(number=17, string="bar"),
+                ],
+                [_MyCandidate(number=19, string="bar"), _MyCandidate(number=2, string="foo")],
+            ),
+        ],
+    )
+    async def test_aggregate_filter(
+        self, filter_under_test: AggregateFilter, candidates: List[dict], survivors: List[dict], caplog
+    ):
+        caplog.set_level(logging.DEBUG, logger=self.__module__)
+        actual = await filter_under_test.apply(candidates)
+        assert actual == survivors
+        assert "There are 4 candidates and 4 aggregates" in caplog.messages
+        assert "There are 2 filtered aggregates left" in caplog.messages
