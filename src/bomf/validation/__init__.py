430196c167dd9e7fe37b56ed32c052984bf9920b
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-03-17 11:34:46 +0100

    Remove obsolet code

--- src/bomf/validation/__init__.py
+++ src/bomf/validation/__init__.py
@@ -3,4 +3,3 @@ bomf performs validation on the intermediate bo4e data set layer. The main class
 """
 
 from bomf.validation.core import ValidatorSet
-from bomf.validation.utils import optional_field, required_field
