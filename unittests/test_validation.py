4a2cd9cb1529b34f0f05cabe81d488ecbcac17f3
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-05-02 15:05:11 +0200

    More coverage

--- unittests/test_validation.py
+++ unittests/test_validation.py
@@ -34,11 +34,17 @@ class DataSetTest(Bo4eDataSet):
 
 class SpecialDataSet(Bo4eDataSet):
     x: list[Wrapper]
+    y: str
+
+
+def iter_list(list_to_iter: list[Any]) -> Iterator[tuple[Any, str]]:
+    return ((el, f"[{index}]") for index, el in enumerate(list_to_iter))
 
 
 dataset_instance = DataSetTest(x="lo16", y=16, z=Wrapper.construct(x="Hello"))
 special_dataset_instance = SpecialDataSet(
-    x=[Wrapper.construct(x="Hello"), Wrapper.construct(x="World"), Wrapper.construct(x="!")]
+    x=[Wrapper.construct(x="Hello"), Wrapper.construct(x="World"), Wrapper.construct(x="!")],
+    y="lul",
 )
 finishing_order: list[ValidatorFunction]
 
@@ -92,11 +98,18 @@ def check_with_param_info(x: str, zz: str = "test"):
     assert zz_param.value == zz and zz == "test"
 
 
-def check_special_data_set(x: str):
+def check_special_data_set(x: str, y: str):
     x_param = param("x")
     assert re.match(r"x\[\d+\]\.x", x_param.param_id)
     assert x_param.provided
     finishing_order.append(check_special_data_set)
+
+
+def check_special_data_set_optional(x: str, y: str = "Hello"):
+    x_param = param("x")
+    assert re.match(r"x\[\d+\]\.x", x_param.param_id)
+    assert x_param.provided
+    finishing_order.append(check_special_data_set_optional)
 
 
 def check_with_param_info_fail(zz: str = "test"):
@@ -159,6 +172,9 @@ validator_check_with_param_info_fail: Validator[DataSetTest, SyncValidatorFuncti
     check_with_param_info_fail
 )
 validator_check_special_data_set: Validator[DataSetTest, SyncValidatorFunction] = Validator(check_special_data_set)
+validator_check_special_data_set_optional: Validator[DataSetTest, SyncValidatorFunction] = Validator(
+    check_special_data_set_optional
+)
 validator_unprovided_but_required: Validator[DataSetTest, SyncValidatorFunction] = Validator(unprovided_but_required)
 validator_check_fail: Validator[DataSetTest, SyncValidatorFunction] = Validator(check_fail)
 validator_check_fail2: Validator[DataSetTest, SyncValidatorFunction] = Validator(check_fail2)
@@ -452,14 +468,37 @@ class TestValidation:
         global finishing_order
         finishing_order = []
 
-        def iter_list(list_to_iter: list[Any]) -> Iterator[tuple[Any, str]]:
-            return ((el, f"[{index}]") for index, el in enumerate(list_to_iter))
-
         validation_manager = ValidationManager[SpecialDataSet]()
         validation_manager.register(
-            QueryMappedValidator(validator_check_special_data_set, {"x": Query().path("x").iter(iter_list).path("x")}),
+            QueryMappedValidator(
+                validator_check_special_data_set,
+                {"x": Query().path("x").iter(iter_list).path("x"), "y": Query().path("y")},
+            ),
         )
         validation_summary = await validation_manager.validate(special_dataset_instance)
         assert validation_summary.num_fails == 0
         assert len(finishing_order) == 3
         assert all(el == check_special_data_set for el in finishing_order)
+
+    async def test_special_data_set_errors(self):
+        global finishing_order
+        finishing_order = []
+
+        validation_manager = ValidationManager[SpecialDataSet]()
+        validation_manager.register(
+            QueryMappedValidator(
+                validator_check_special_data_set,
+                {"x": Query().path("x").iter(iter_list).path("x"), "y": Query().path("y")},
+            ),
+        )
+        validation_manager.register(
+            QueryMappedValidator(
+                validator_check_special_data_set_optional,
+                {"x": Query().path("x").iter(iter_list).path("x"), "y": Query().path("y")},
+            ),
+        )
+        validation_summary = await validation_manager.validate(SpecialDataSet.construct(x=special_dataset_instance.x))
+        assert validation_summary.num_errors_total == 1
+        assert "y not provided" in str(validation_summary.all_errors[0])
+        assert len(finishing_order) == 3
+        assert all(el == check_special_data_set_optional for el in finishing_order)
