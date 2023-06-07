0a93ad11f1d27db73f63541319ad1fe4efbea4af
konstantin <konstantin.klein@hochfrequenz.de>
2023-06-08 00:47:04 +0200

    Add convenience function for easier mapping (sync)

--- unittests/test_list_conversion.py
+++ unittests/test_list_conversion.py
@@ -1,10 +1,13 @@
 import logging
 
-from bomf.mapper import convert_single_mapping_task_into_list_mapping_task_with_single_pokemon_catchers
+from bomf.mapper import (
+    convert_single_mapping_into_list_mapping_with_single_pokemon_catchers,
+    convert_single_mapping_task_into_list_mapping_task_with_single_pokemon_catchers,
+)
 
 
 class TestListMappingConversion:
-    async def test_conversion(self, caplog):
+    async def test_conversion_async(self, caplog):
         async def mapping_func(x: int) -> str:
             if x == 3:
                 raise Exception("Fatal crash")
@@ -14,5 +17,18 @@ class TestListMappingConversion:
         logger = logging.getLogger("foo")
         actual = convert_single_mapping_task_into_list_mapping_task_with_single_pokemon_catchers(mapping_func, logger)
         test_result = await actual([1, 2, 3, 4, 5])
+        assert test_result == ["1", "2", "4", "5"]
+        assert caplog.messages[0] == "Error while calling mapping_func on 3: Fatal crash"
+
+    def test_conversion_sync(self, caplog):
+        def mapping_func(x: int) -> str:
+            if x == 3:
+                raise Exception("Fatal crash")
+            return str(x)
+
+        caplog.set_level(logging.ERROR, "foo")
+        logger = logging.getLogger("foo")
+        actual = convert_single_mapping_into_list_mapping_with_single_pokemon_catchers(mapping_func, logger)
+        test_result = actual([1, 2, 3, 4, 5])
         assert test_result == ["1", "2", "4", "5"]
         assert caplog.messages[0] == "Error while calling mapping_func on 3: Fatal crash"
