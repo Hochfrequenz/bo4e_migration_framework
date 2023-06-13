cfb576b42be5ce7dd46c569502f44c79bda55341
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-06-13 13:52:44 +0200

    Redesign logging teardown

--- src/bomf/logging/__init__.py
+++ src/bomf/logging/__init__.py
@@ -1,19 +1,21 @@
 import logging
 from contextvars import ContextVar, Token
+from typing import Callable
 
 logger: ContextVar[logging.Logger] = ContextVar("logger", default=logging.getLogger("bomf-unbound"))
 
 
-def initialize_logger(context_specific_logger: logging.Logger) -> Token:
+def initialize_logger(context_specific_logger: logging.Logger) -> Callable[[], None]:
     """
-    Initialize the logger context variable. You should use the returned token to clear the context variable after the
-    migration to prevent too much memory usage.
+    Initialize the logger context variable. You should use the returned teardown function to clear the context variable
+    after the migration to prevent too much memory usage.
     """
-    return logger.set(context_specific_logger)
+    token = logger.set(context_specific_logger)
 
+    def clear_logger():
+        """
+        Clear the logger context variable. You should call this method after the migration to prevent too much memory usage.
+        """
+        logger.reset(token)
 
-def clear_logger(token: Token):
-    """
-    Clear the logger context variable. You should call this method after the migration to prevent too much memory usage.
-    """
-    logger.reset(token)
+    return clear_logger
